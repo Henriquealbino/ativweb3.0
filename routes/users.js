b2ad16cad5853f8ba046822fcf7650ca967b4e53
Henriquealbino <henriquemauriciomagnus@gmail.com>
2023-10-06 10:56:18 -0300

    meu primeiro commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-const express = require("express");
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
-
+/* GET home page. */
+router.get('/cool', function(req, res, next) {
+  res.send('you are so cool');
+});
+/* GET home page. */
+router.get('/beach/cool', function(req, res, next) {
+  res.send('beach is so cool');
+});
+/* GET home page. */
+router.get('/ifc', function(req, res, next) {
+  res.send('ifc is very hard');
+});
 module.exports = router;
