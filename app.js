2b0e7c4c486f869e3b77c3d7ac977004de148174
Henriquealbino <henriquemauriciomagnus@gmail.com>
2023-10-10 08:42:14 -0300

    bosta

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const compression = require("compression");
 // Create the Express application object
 const app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 // …
 
 app.use(compression()); // Compress all routes
@@ -31,9 +40,20 @@ var mongoose = require("mongoose");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
 const dev_db_url =
-  "mongodb+srv://henriquemauriciomagnus:Ds7DOKxY0X0O50DH@cluster0.acwipvr.mongodb.net/?retryWrites=true&w=majority";
+"mongodb+srv://henriquemauriciomagnus:Ds7DOKxY0X0O50DH@cluster0.acwipvr.mongodb.net/?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+  
 
 main().catch((err) => console.log(err));
 async function main() {
